da6f158f3a3fdaaf124b443a2b9d5e1d99b30eaa
23f3000211 <23f3000211@ds.study.iitm.ac.in>
2026-07-08 21:41:39 +0530

    Update comment for FastAPI application start command

--- startt.sh
+++ startt.sh
@@ -2,5 +2,5 @@
 # Start Redis server in the background
 redis-server --daemonize yes
 
-# Start the FastAPI application on the port provided by Render (default 10000)
+# Start the FastAPI application on the port provided by Render
 uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
